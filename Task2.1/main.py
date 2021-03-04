7c4fa486050b2a9f80fb891246d3f4715818808a
LustLoop <lustloop@gmail.com>
2021-03-04 16:27:53 +0200

    Solved task2.1

---
+++ Task2.1/main.py
@@ -0,0 +1,6 @@
+import task1
+
+
+if __name__ == '__main__':
+    task1.solve()
+
---
+++ Task2.1/task1.py
@@ -0,0 +1,16 @@
+import random
+
+
+def solve():
+    random_list = []
+
+    n = int(input('Enter value of "n"'))
+    a = int(input('Enter value of "a"'))
+    b = int(input('Enter value of "b"'))
+
+    for i in range(0, n):
+        x = random.randint(a, b)
+        random_list.append(x)
+
+    print(random_list)
+
