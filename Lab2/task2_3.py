8603f62c2be1243f625a6a89139f97ae26170ca5
LustLoop <lustloop@gmail.com>
2021-03-05 19:41:27 +0200

    Fix task numeration

--- Lab2/task2_3.py
+++ Lab2/task2_3.py
@@ -1,10 +1,1 @@
-def solve():
-    input_list = [5, -5, 3, 7, -7, 10]
-    print(input_list)
-    x = int(input('Enter value of "x"'))
-    n = int(input('Enter value of "n"'))
-    result = input_list[:n] + [x] + input_list[n:]
-    print(result)
 
-
-solve()
