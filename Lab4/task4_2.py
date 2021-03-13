2277a10f4a7b1ace47c133789488a4f651b0ea40
LustLoop <lustloop@gmail.com>
2021-03-14 01:41:18 +0200

    Add task 4.2

---
+++ Lab4/task4_2.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def solve():
+    m = int(input('Enter M'))
+    n = int(input('Enter N'))
+
+    new_matrix = np.empty([m, n])
+
+    new_matrix.fill(0.5)
+    np.fill_diagonal(new_matrix, -1)
+
+    print(new_matrix)
+
+    coordinate1 = convert_to_coordinates(input('Enter left-top corner'))
+    coordinate2 = convert_to_coordinates(input('Enter right-bottom corner'))
+
+    print(new_matrix[np.ix_(coordinate1, coordinate2)])
+
+
+def convert_to_coordinates(string):
+    result = []
+    list_of_strings = string.split(' ')
+    for string in list_of_strings:
+        result.append(int(string))
+    print(result)
+    return result
+
+
+solve()
