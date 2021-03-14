f17127cef4b863289d58280a97393e2327030840
LustLoop <lustloop@gmail.com>
2021-03-14 17:38:06 +0200

    Adapt input for task 4.3

--- Lab4/task4_3.py
+++ Lab4/task4_3.py
@@ -2,9 +2,8 @@ import numpy as np
 
 
 def solve():
-    first_array = input('Enter first array')
+    first_array_of_ints = input('Enter first array').split(' ')
     indexes_array = input('Enter second array')
-    first_array_of_ints = convert_to_list_of_ints(first_array)
     indexes_array_of_ints = convert_to_list_of_ints(indexes_array)
     matrix = np.asarray(first_array_of_ints)
     print(matrix)
